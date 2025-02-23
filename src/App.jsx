2ae81f312e54e6c1ffb7e023be6d5412b2fa278d
Roman <roman-meinl@ukr.net>
2025-02-23 15:58:13 -0200

    create function ChangeText

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import { ways } from "./data";
+import ChangeText from "./components/ChangeText";
+
 
 function App() {
   return (
     <div>
-     <Header />
-        <main>
+      <Header />
+      <main>
+        <section>
           <h1>Hello React!!!</h1>
-        </main>
-      
+
+          <p>JavaScript-библиотека для создания пользовательских интерфейсов</p>
+          <ul>
+            {/*<ChangeText{...ways[0]}/>----- это короткая запись того что ниже*/}
+            <ChangeText
+              title={ways[0].title}
+              description={ways[0].description}
+            />
+            {/*<ChangeText{...ways[1]}/>----- это короткая запись того что ниже*/}
+            <ChangeText
+              title={ways[1].title}
+              description={ways[1].description}
+            />
+            {/*<ChangeText{...ways[2]}/>----- это короткая запись того что ниже*/}
+            <ChangeText
+              title={ways[2].title}
+              description={ways[2].description}
+            />
+            {/*<ChangeText{...ways[3]}/>----- это короткая запись того что ниже*/}
+            <ChangeText
+              title={ways[3].title}
+              description={ways[3].description}
+            />
+          </ul>
+        </section>
+        <section>
+          <h3>
+            
+          </h3>
+        </section>
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 
-// создаем функцию которая возвращает header
+import logo from '/vite.svg'
 
+// создаем функцию которая возвращает header
 export default function Header(){
     //Получаем время
     const now = new Date();
     const hours = now.getHours();
     const minutes = now.getMinutes()
     const getFullTime = `${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
-    
+
     return (
       <header>
+        <img src={logo} alt={"logo"} />
       <h3>First lesson on React</h3>
       <span>Start 23.02.25</span>
       <span>Time now :{getFullTime}</span>
