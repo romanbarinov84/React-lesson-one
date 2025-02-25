e60c2c3afe19a4c7f1bf9552b7a749a679ab0540
Roman <roman-meinl@ukr.net>
2025-02-25 17:26:42 -0200

    ss

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-lesson-one/',
   server: {
     open: true, // Автоматически откроет браузер
   },
